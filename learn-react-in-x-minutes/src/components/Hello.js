96d744b4b339ce7305e61c4976157f219e48f121
TKabakov <boteobg@gmail.com>
2023-04-02 08:30:26 +0300

    Writing component without JSX

--- learn-react-in-x-minutes/src/components/Hello.js
+++ learn-react-in-x-minutes/src/components/Hello.js
@@ -1,11 +1,12 @@
 import React from "react"
 
 const Hello = () => {
-    return (
-        <div>
-            <h2>Hello, Developer!</h2>
-        </div>
-    )
+//    return (
+//        <div>
+//            <h2>Hello, Developer!</h2>
+//        </div>
+//    )
+    return React.createElement("div", null, "Hello, great developer!") 
 }
 
 export default Hello 
