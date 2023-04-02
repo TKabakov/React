fe5be4b791e5a6d44ec19cd8fe55c27e248f3342
TKabakov <boteobg@gmail.com>
2023-04-02 08:48:33 +0300

    Final product

--- learn-react-in-x-minutes/src/components/Hello.js
+++ learn-react-in-x-minutes/src/components/Hello.js
@@ -1,16 +1,20 @@
 import React from "react"
 
 const Hello = () => {
+//Class component using JSX:
 //    return (
-//        <div>
+//        <div className="dummyClass">
 //            <h2>Hello, Developer!</h2>
 //        </div>
 //    )
+
+//Class component without JSX:
     return React.createElement(
         "div", 
-        null, 
+        {id: "Hello", className: "dummyClass"}, 
         React.createElement("h1", null, "Hello, great developer!")
-     ) 
+     )
+//Every JSX is calling React.createElement()
 }
 
 export default Hello 
