b47a839bd3498766c2f3e1e2bc616196a122013a
TKabakov <boteobg@gmail.com>
2023-04-04 19:37:36 +0300

    Creating js file for ternary operator

---
+++ ternary operator.js
@@ -0,0 +1,20 @@
+/*let name = "Bob";
+if( name == "Bob"){
+    console.log("Hello, "+"Bob");
+}
+else {
+    console.log("Hello, Friend!");
+}
+*/
+
+let name;
+name = 'Bob';
+/*
+if (name == 'Bob') {
+    console.log('Hello, Bob');
+} else {
+    console.log('Hello, Friend');
+};
+*/
+
+name == "Bob"? console.log("Hello, Bob") : console.log("Hello, Friend!");
