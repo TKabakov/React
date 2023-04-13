b43d3205c7b1cbe057285ec8c8321dc94d8aede3
TKabakov <boteobg@gmail.com>
2023-04-13 09:11:12 +0300

    Adding handleClick function

--- user-event/src/ModeToggler.js
+++ user-event/src/ModeToggler.js
@@ -1,11 +1,23 @@
 function ModeToggler(){
-    const darkModeOn = true;
+    let darkModeOn = true;
     const darkMode = <h1> Dark Mode is On</h1>;
     const lightMode = <h1> Light Mode is On </h1>;
     
+    function handleClick(){
+        darkModeOn = !darkModeOn;
+        if (darkModeOn == true) {
+            console.log ("Dark mode is on")
+        } else {
+            console.log ("Light mode is on")
+        }
+    }
+
     return(
         <div>
-         {darkModeOn ? darkMode : lightMode}
+            {darkModeOn ? darkMode : lightMode}
+            <button onClick={handleClick}>
+                Click me
+            </button>
         </div>
         );
 };
