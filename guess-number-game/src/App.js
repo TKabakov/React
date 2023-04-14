69dddba0b1e23f93814b1a79cf27691aeb467186
TKabakov <boteobg@gmail.com>
2023-04-14 22:54:04 +0300

    Creating ne component and updating the App.js

--- guess-number-game/src/App.js
+++ guess-number-game/src/App.js
@@ -14,6 +14,9 @@ function App() {
       <h1>Task: Add a button and handle a click event</h1>
       <button onClick ={handleClick}>Guess the number between 1 and 3</button>
     </div>
+    <div className="DataFlowInReact">
+      <Puppy name="Max" bowlHSape="Square" bowlStatus="full"/>
+    </div>
   );
 }
 
---
+++ guess-number-game/src/components/Puppy.js
@@ -0,0 +1,7 @@
+function Puppy (props) {
+    return (
+        <div>
+            {props.name} has <Bowl bowlShape="square" bowlStatus="full"
+        </div>
+    )
+}
