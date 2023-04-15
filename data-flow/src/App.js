c7a119a5b92f65d66daf3da836554492afdd80ea
TKabakov <boteobg@gmail.com>
2023-04-15 07:24:01 +0300

    Remove the values of the attributes in App.js that are already available in Puppy.js

--- data-flow/src/App.js
+++ data-flow/src/App.js
@@ -7,7 +7,7 @@ import Puppy from './components/Puppy';
 function App() {
   return (
     <div>
-      <Puppy name="Teo" bowlShape="square" bowlStatus="full"/>
+      <Puppy name="Teo" bowlShape bowlStatus/>
     </div>
   //Check the bowlShape and bowlStatus props in the child Puppy component
   );
--- data-flow/src/components/Puppy.js
+++ data-flow/src/components/Puppy.js
@@ -3,7 +3,7 @@ import Bowl from './Bowl'
 function Puppy (props) {
     return (
         <div>
-            {props.name} has <Bowl bowlShape="circle" bowlStatus="empty"/>
+            {props.name} has <Bowl bowlShape="square" bowlStatus="empty"/>
         </div>
     );
 };
