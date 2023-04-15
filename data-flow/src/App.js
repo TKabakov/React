01c57af614a6cd3f4cccce37aca5ada0979873f2
TKabakov <boteobg@gmail.com>
2023-04-15 07:52:43 +0300

    Adding Child component and import it to App.js to show current time

--- data-flow/src/App.js
+++ data-flow/src/App.js
@@ -1,12 +1,14 @@
-
-import './App.css';
-
 import './App.css';
 import Puppy from './components/Puppy';
+import Child from './components/Child';
 
 function App() {
+  const date = new Date();
   return (
     <div>
+      <div>
+        <Child message={date.toLocaleTimeString()}/>
+      </div>
       <Puppy name="Teo" bowlShape bowlStatus/>
     </div>
   //Check the bowlShape and bowlStatus props in the child Puppy component
