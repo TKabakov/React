7ac4909318d9d3ba19f640b97c5a6446f6a504ea
TKabakov <boteobg@gmail.com>
2023-04-16 13:03:19 +0300

    Creating RegistrationFrom component

---
+++ data-flow/src/components/RegistrationForm.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function RegistationForm () {
+    const [] = useState ();
+
+    return (
+        <>
+            <input type="text "value></input>
+            <input type="text" value></input>
+            <input type="email"></input>
+        </>
+    )
+}
