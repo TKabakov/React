0456137dbe2fcda5f7b6ceb145e097c68e9ed571
TKabakov <boteobg@gmail.com>
2023-04-18 17:02:23 +0300

    Updating the App.js

--- data-flow/src/App.js
+++ data-flow/src/App.js
@@ -5,6 +5,7 @@ import InputComponent from './components/InputComponent';
 import RegistrationForm from './components/RegistrationForm';
 import TextInputWithFocusButton from './components/TextInputWithFocusButton'
 import React, { useState } from 'react';
+import HookCounter from './components/HookCounter';
 
 function countInitial () {
   console.log('run function')
@@ -42,6 +43,9 @@ function App() {
       </div>
       <div>
         <TextInputWithFocusButton/>
+      </div>
+      <div>
+        <HookCounter/>
       </div>
     </div>
   //Check the bowlShape and bowlStatus props in the child Puppy component
--- data-flow/src/components/HookCounter.js
+++ data-flow/src/components/HookCounter.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 
 function HookCounter() {
-    useState(0)
+    
+    const [count, setCount] = useState(0)
 
   return (
     <div>
-        <button onClick={}>Count {}</button>
+        <button onClick={() => setCount(count + 1)}>Count {count}</button>
     </div>
   )
 }
 
-export default HookCounter
+export default HookCounter;
