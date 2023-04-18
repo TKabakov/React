f71bf3238cc5ed7b8b49fd5bd23220aa5198fe60
TKabakov <boteobg@gmail.com>
2023-04-18 16:54:01 +0300

    Creating a functional component

---
+++ data-flow/src/components/HookCounter.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function HookCounter() {
+    useState(0)
+
+  return (
+    <div>
+        <button onClick={}>Count {}</button>
+    </div>
+  )
+}
+
+export default HookCounter
