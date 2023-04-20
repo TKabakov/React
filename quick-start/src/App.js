6020095bca1ff5a81ac5deb7a70eeeb959c22b43
TKabakov <boteobg@gmail.com>
2023-04-20 19:49:51 +0300

    Creating AboutPage component with br closing tag

--- quick-start/src/App.js
+++ quick-start/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import AboutPage from './components/AboutPage';
 import MyButton from './components/MyButton';
 
 function App() {
   return (
     <div className="App">
+      <h1>Werlcome to my app</h1>
       <MyButton />
+      <AboutPage />
     </div>
   );
 }
