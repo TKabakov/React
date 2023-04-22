9d3ef540e907e0f7bc6d3c73b2c0d276311e7bc1
TKabakov <boteobg@gmail.com>
2023-04-22 21:22:33 +0300

    Creating Profile component

--- quick-start/src/App.js
+++ quick-start/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import AboutPage from './components/AboutPage';
 import MyButton from './components/MyButton';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>Werlcome to my app</h1>
       <MyButton />
       <AboutPage />
+      <Profile />
     </div>
   );
 }
