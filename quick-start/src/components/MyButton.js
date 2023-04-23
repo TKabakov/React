0c28d7e806c12ae0906c77c8ec323678854a9f37
TKabakov <boteobg@gmail.com>
2023-04-23 20:45:41 +0300

    Adding state to the button

--- quick-start/src/components/MyButton.js
+++ quick-start/src/components/MyButton.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
+
 function MyButton(){
+    const[ count, setCount ] = useState(0);
+
     function handleClick() {
-        alert ('You clicked me!');
+        alert ('You clicked the button!');
+
+        setCount(count+1);
+
     }
     return (
-        <button onClick={handleClick}>I'm a button</button>
+        <button onClick={handleClick}>
+            Clicked {count} times!
+            </button>
     );
 };
 
