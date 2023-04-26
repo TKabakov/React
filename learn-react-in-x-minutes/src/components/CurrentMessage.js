0cb8b7e77ec8a501873a13b8c370ceb4e25e06c6
TKabakov <boteobg@gmail.com>
2023-04-26 20:10:32 +0300

    Adding ErrorComponent

--- learn-react-in-x-minutes/src/components/CurrentMessage.js
+++ learn-react-in-x-minutes/src/components/CurrentMessage.js
@@ -1,17 +1,20 @@
+import ErrorComponent from "./components/ErrorComponent";
 import Weekends from "./components/Weekends";
 import Workdays from "./components/Workdays";
 
 function CurrentMessage() {
     const day = new Date().getDay();
         const weekday = (day >= 1 && day <= 5);
-        
+        const weekend = (day >= 6 && day <= 7);
         let message;
     
         if (weekday) {
             message = <Workdays />
-        } else {
+        } else if (weekend){
             message = <Weekends />
-        }
+        } else {
+            message = <ErrorComponent />
+    }
     
         return (
             <div>
