cbeee791d793ce24254e3263d7ad4415c4ce5257
TKabakov <boteobg@gmail.com>
2023-04-27 18:58:51 +0300

    Adding ErrorPage component

--- name-of-your-project/src/main.jsx
+++ name-of-your-project/src/main.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
   createBrowserRouter,
+  Route,
   RouterProvider,
 } from "react-router-dom"
 import './index.css'
 import Root from "./routes/root";
+import ErrorPage from './error-page';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
