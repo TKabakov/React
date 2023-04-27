fcb19e1716a8ae797fcb3ef20fad915dfd79c6e9
TKabakov <boteobg@gmail.com>
2023-04-27 21:27:29 +0300

    That's it

--- name-of-your-project/src/main.jsx
+++ name-of-your-project/src/main.jsx
@@ -9,12 +9,17 @@ import './index.css'
 import Root from "./routes/root";
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
+import {  
+        loader as rootLoader,
+        action as rootAction 
+      } from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "contacts/:contactId",
