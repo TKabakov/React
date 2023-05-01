72ad892762b7ccaafb7945a63db9cc9ef98af074
TKabakov <boteobg@gmail.com>
2023-05-01 20:29:54 +0300

    Adding routes for Home, Pricing, About

--- router/src/App.js
+++ router/src/App.js
@@ -1,10 +1,29 @@
 import Navbar from './Navbar';
 import './App.css';
+import Home from './pages/Home';
+import Pricing from './pages/Pricing';
+import About from './pages/About';
 
 function App() {
+  let Component
+  switch(window.location.pathname){
+    case "/":
+      Component = Home
+      break
+    case "/pricing":
+      Component = Pricing
+      break
+    case "/about":
+      Component = About
+      break
+  }
   return (
     <>
     <Navbar />
+    <div className="container">
+      <Component/>
+    </div>
+    
     </>
   );
 }
