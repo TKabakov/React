55371c8cd841881bc0b8c7bf99de432b8fdd3d6e
TKabakov <boteobg@gmail.com>
2023-05-04 21:25:06 +0300

    Adding audio

--- using-assets/src/App.js
+++ using-assets/src/App.js
@@ -1,10 +1,37 @@
+import React from "react";
 import logo from './assets/logo.png';
 
 //Dispalying an image using import
 //Diplaying an image using the require function
+//Displaying an image using URL
 
 function App() {
   const profileImg = "https://picsum.photos/400/265";
+
+  const bird1 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
+  );
+
+  const bird2 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3"
+  );
+
+  function toggle1() {
+    if (bird1.paused) {
+      bird1.play();
+    } else {
+      bird1.pause();
+    }
+  };
+
+  function toggle2() {
+    if (bird2.paused) {
+      bird2.play();
+    } else {
+      bird2.pause();
+    }
+  };
+
   return (
     <div className="App">
       <h1>Task: Add an image below</h1>
@@ -23,6 +50,10 @@ function App() {
         src={ profileImg }
         alt="Logo"
       />
+      <div>
+        <button onClick={toggle1}>Caspian Tern 1</button>
+        <button onClick={toggle2}>Caspian Tern 2</button>
+      </div>
     </div>
   );
 }
