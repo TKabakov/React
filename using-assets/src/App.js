7c33dc972b692b0945fbabeaaf7c8f6d95ff9427
TKabakov <boteobg@gmail.com>
2023-05-04 09:42:27 +0300

    Displaying image using URL

--- using-assets/src/App.js
+++ using-assets/src/App.js
@@ -4,6 +4,7 @@ import logo from './assets/logo.png';
 //Diplaying an image using the require function
 
 function App() {
+  const profileImg = "https://picsum.photos/400/265";
   return (
     <div className="App">
       <h1>Task: Add an image below</h1>
@@ -15,6 +16,11 @@ function App() {
       <img
         width={1000}
         src={require("./assets/logo.png")}
+        alt="Logo"
+      />
+      <img
+        width={1000}
+        src={ profileImg }
         alt="Logo"
       />
     </div>
