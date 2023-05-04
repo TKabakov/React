afbeb4ee178419761deea68a1e7b2753b37b9585
TKabakov <boteobg@gmail.com>
2023-05-04 09:31:42 +0300

    Displaying image using import and require

--- using-assets/src/App.js
+++ using-assets/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.png';
+
+//Dispalying an image using import
+//Diplaying an image using the require function
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task: Add an image below</h1>
+      <img
+        width={800}
+        src={logo}
+        alt="Logo"
+      />
+      <img
+        width={1000}
+        src={require("./assets/logo.png")}
+        alt="Logo"
+      />
     </div>
   );
 }
