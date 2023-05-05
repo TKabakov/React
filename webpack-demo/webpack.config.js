454ae3adfb9fe7a633fb6cd13e5a8c9faf837415
TKabakov <boteobg@gmail.com>
2023-05-05 19:49:02 +0300

    Greating config file

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,9 @@
+const path = rewuire('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
