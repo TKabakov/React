37f5160000fd055dd9e25367bf6c4afe1b8c1e75
TKabakov <boteobg@gmail.com>
2023-05-06 06:36:22 +0300

    Loading CSS

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,17 @@
-const path = rewuire('path');
+const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
 };
