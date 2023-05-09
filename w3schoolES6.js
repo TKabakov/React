0f88764d43abf53219518a40f5be4274e542caca
TKabakov <boteobg@gmail.com>
2023-05-09 18:59:27 +0300

    Destructing

--- w3schoolES6.js
+++ w3schoolES6.js
@@ -1,3 +1,4 @@
+//Arrow functions
 let hello = function() {
     return "Hello World!";
   }
@@ -16,3 +17,21 @@ console.log(hello3());
 
 let hello4 = (val) => "Hello" + val;
 console.log(hello4(" World!"));
+
+//Destructing 
+function calculate (a,b) {
+  const add = a + b;
+  const substract = a -b;
+  const multiplay = a * b;
+  const divide = a / b;
+
+  return [add, substract, multiplay, divide]
+}
+
+const [add, substract, multiplay, divide] = calculate(5,8);
+console.log("Sum: " + add);
+console.log("Substarct: " + substract);
+console.log("Multiplay: " + multiplay);
+console.log("Divide: "+ divide);
+
+
