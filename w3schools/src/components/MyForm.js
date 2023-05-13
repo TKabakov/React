a613a73c54b0823cae00c8a694140b6d5d1582a0
TKabakov <boteobg@gmail.com>
2023-05-13 18:28:25 +0300

    Added second field number

--- w3schools/src/components/MyForm.js
+++ w3schools/src/components/MyForm.js
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 
 function MyForm() {
-    const [name, setName] = useState("");
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
     
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert(`The name you entered was: ${name}`)
+        console.log(inputs);
     }
 
     return (
@@ -13,8 +19,17 @@ function MyForm() {
             <label>Enter your name:
                 <input
                     type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
+                    name="username"
+                    value={inputs.username || ""}
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Enter your age:
+                <input
+                    type="number"
+                    name="age"
+                    value={inputs.age || ""}
+                    onChange={handleChange}
                 />
             </label>
             <input type="submit" />
