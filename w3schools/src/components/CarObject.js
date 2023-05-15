0c463fd9a6a30ac9744105bdaf81d351f779ab92
TKabakov <boteobg@gmail.com>
2023-05-15 17:23:50 +0300

    updateColor  function added

--- w3schools/src/components/CarObject.js
+++ w3schools/src/components/CarObject.js
@@ -8,12 +8,22 @@ function MyCar () {
         color: "red"
     });
 
+    const updateColor = () => {
+        setCar(previousState => {
+            return { ...previousState, color:"blue"}
+        });
+    }
+
     return(
         <>
             <h1>My {car.brand}</h1>
             <p>
                 It is a {car.color} {car.model} from {car.year}.
             </p>
+            <button
+                type="button"
+                onClick={updateColor}
+            >Blue</button>
         </>
     );
 };
