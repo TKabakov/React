809473adde967392c98e888362cf8fb4b856e63f
TKabakov <boteobg@gmail.com>
2023-05-20 06:49:05 +0300

    Some udates

--- w3schools/src/components/DessertList.js
+++ w3schools/src/components/DessertList.js
@@ -1,13 +1,13 @@
+
 function DessertsList(props) {
     const listItems = props.data.map(dessert => {
       const itemText = `${dessert.name} - ${dessert.calories}`
       return <li>{itemText}</li>
     })
     
-    const listFilter = listItems.filter(item => item.calories < 500)
     return (
       <ul>
-       { listFilter }
+       { listItems }
       </ul>
     );
   }
