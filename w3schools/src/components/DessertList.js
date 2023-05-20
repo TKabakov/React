406fb8a095c61d2062ae7860a6e08eb588bf45df
TKabakov <boteobg@gmail.com>
2023-05-20 07:36:15 +0300

    Filter and Sort are chained in one statement

--- w3schools/src/components/DessertList.js
+++ w3schools/src/components/DessertList.js
@@ -1,15 +1,20 @@
-
 function DessertsList(props) {
+   /*
     const filterItems = props.data.filter(item => item.calories < 500);
 
     const sortItems = filterItems.sort((a,b) => a.calories-b.calories);
+    */
 
-    const listItems = sortItems.map(dessert => {
+    const filterAndSort = props.data.filter(item => item.calories < 500)
+    .sort((a,b) => a.calories-b.calories);
+
+    //Filter and Sort are chained in one statement;
+
+    const listItems = filterAndSort.map(dessert => {
       const itemText = `${dessert.name} - ${dessert.calories}`
       return <li>{itemText}</li>
     })
     
-
     return (
       <ul>
        { listItems }
