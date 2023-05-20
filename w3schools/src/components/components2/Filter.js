99f9cf66e3f6c77485f09aff8707c14eb16a7ddb
TKabakov <boteobg@gmail.com>
2023-05-20 14:28:23 +0300

    Fillter.js is removed

---
+++ w3schools/src/components/components2/Filter.js
@@ -0,0 +1,15 @@
+const numbers = [ 2, 3, 4, 5, 6];
+
+const filter = numbers.filter(filterNumbers);
+
+function filterNumbers (n){
+  return n < 5
+};
+
+console.log(filter);
+
+console.log(numbers);
+
+
+
+
