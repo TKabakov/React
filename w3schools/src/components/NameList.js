0272ca11bee93a1e8b4537f7872cb84b7ed50398
TKabakov <boteobg@gmail.com>
2023-05-22 20:25:51 +0300

    Redering list of objects

--- w3schools/src/components/NameList.js
+++ w3schools/src/components/NameList.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
 function NameList() {
-    const names = ['Bruce', 'Clark', 'Diana'];
+    const persons = [
+        {   
+            id : 1,
+            name : "Bruce", 
+            age : 30,
+            skill: "React"},
+        {
+            id : 2,
+            name : "Clark",
+            age: 25,
+            skill: 'Angular'
+        },
+        {
+            id : 3,
+            name : "Diana",
+            age: 28,
+            skill: 'Vue'
+        }
+    ];
 
-    const nameList = names.map(name => <h2>{name}</h2>);
+    const nameList = persons.map(person => <h2>{person.name} is a {person.skill} developer at an age of {person.age}.</h2>);
   return (<div>{ nameList }</div>)
 }
 
