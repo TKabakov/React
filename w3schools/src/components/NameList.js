221ce5a64c376baa9f0a64bf0a8f534ae9562380
TKabakov <boteobg@gmail.com>
2023-05-22 20:15:31 +0300

    Syntax ipdated

--- w3schools/src/components/NameList.js
+++ w3schools/src/components/NameList.js
@@ -1,14 +1,10 @@
 import React from 'react'
 
 function NameList() {
-    const names = ['Bruce', 'Clark', 'Diana']
-  return (
-    <div>
-        {
-            names.map(name => <h2>{name}</h2>)
-        }
-    </div>
-  )
+    const names = ['Bruce', 'Clark', 'Diana'];
+
+    const nameList = names.map(name => <h2>{name}</h2>);
+  return (<div>{ nameList }</div>)
 }
 
 export default NameList
