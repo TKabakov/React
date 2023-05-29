ad273acb621b11ae994e00dfd32fb10a8340cda6
TKabakov <boteobg@gmail.com>
2023-05-29 16:28:18 +0300

    Ul code for recipe names added

--- w3schools/src/components/components2/Salads.js
+++ w3schools/src/components/components2/Salads.js
@@ -21,57 +21,5 @@ export default function Recipes (){
   )
 }
 
-/*
-export default Recipes (){
-const Names = recipes.map((name) => {
-  return <li key={name.id}>{name.name}</li>
-})
-
-return (
-  <div>
-    <h2>List of low calories desserts:</h2>
-    <ul>
-        { Names }
-    </ul>
-  </div>
-
-);
-}
-*/
-
-/*
-export default function Recipes (){
-  recipes.map((item) => {
-  return (
-    <ul>{item.name}
-    {item.ingredients.map(child => (
-     <li>{child.ingrediants}</li>
-    ))}
-   </ul>
-);})}
-*/
-//Nested Array of Object
-/*
-export const  testData = [{
-
-    SectionName: "Product", items: [
-      { "id": 1, "Name": "MacBook" },
-      { "id": 2, "Name": "iPhone" },
-      { "id": 3, "Name": "iPad" },
-      { "id": 4, "Name": "iWatch" }
-    ]
-  },
-  {
-    SectionName: "Company", items: [
-      { "id": 1, "Name": "Google" },
-      { "id": 2, "Name": "FaceBook" },
-      { "id": 3, "Name": "IBM" },
-      { "id": 4, "Name": "Cisco" }
-
-    ]
-  }
-];
-*/
-
 
 
