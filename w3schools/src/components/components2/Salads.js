4fc24059a16e5585b82f92e255e1a2d83d0c4b0b
TKabakov <boteobg@gmail.com>
2023-05-30 06:45:12 +0300

    Finally I have anested array mapped

--- w3schools/src/components/components2/Salads.js
+++ w3schools/src/components/components2/Salads.js
@@ -1,7 +1,25 @@
 import { recipes } from "./SaladsData";
 
 export default function Recipes (){
-  
+  return (
+    <div>
+      {recipes.map((recipe) =>{
+        return <div key={recipe.id}>
+        <h1>{recipe.name}</h1>
+        {recipe.ingredients.map((type, index) =>{
+          return <li key={index}>{type}</li>
+        }
+          )
+        }
+        </div>
+      }
+  )
+      }
+    </div>
+  )
+    }
+
+    /*
   const recipe = recipes.map(
   (recipe) => {
     return(
@@ -29,5 +47,5 @@ export default function Recipes (){
   )
 }
 
-
+*/
 
