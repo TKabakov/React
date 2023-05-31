06eae3513b02664e631a5c2f838600188b112a41
TKabakov <boteobg@gmail.com>
2023-05-31 06:39:43 +0300

    Component Recipeis add

---
+++ w3schools/src/components/components2/Recipe.js
@@ -0,0 +1,21 @@
+import { recipes } from "./SaladsData";
+
+export default function RecipeList(){
+    return (
+        <div>
+            <h1>Recipes</h1>
+            {recipes.map(recipe =>
+                <div key={recipes.id}>
+                    <h2>{recipe.name}</h2>
+                    <ul>
+                        {recipe.ingredients.map(ingredient =>
+                            <li key={ingrediant}>
+                                {ingredient}
+                            </li>
+                            )}
+                    </ul>
+                    )}
+                </div>
+        </div>
+    );
+}
