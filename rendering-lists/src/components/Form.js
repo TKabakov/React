be1e4cce3f5d99e15cbe14ac4d026ad61438bc55
TKabakov <boteobg@gmail.com>
2023-06-03 08:07:41 +0300

    From updated

--- rendering-lists/src/components/Form.js
+++ rendering-lists/src/components/Form.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useRef } from 'react';
 
 function Form() {
+    const inputRef = useRef(null);
+
+    const handleSubmit = () => {
+        const inputValue = inputRef.current.value;
+    }
+
   return (
     <div>
-      <div>
-        <input type='text'/>
-      </div>
+        <form onSubmit={handleSubmit}>
+            <input ref={inputRef} type='text' />
+        </form>
     </div>
   )
 }
