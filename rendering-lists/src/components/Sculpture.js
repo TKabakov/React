337a1fb23b0585e01c2a56628c693400e2c02839
TKabakov <boteobg@gmail.com>
2023-06-10 21:36:27 +0300

    New component Ssculpture is added

---
+++ rendering-lists/src/components/Sculpture.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState } from 'react';
+import { sculptureList } from './sculptureList';
+
+function Sculpture() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Sculpture
