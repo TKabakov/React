767fcd6aa4655e5c41a338a8eb511bc233feac47
TKabakov <boteobg@gmail.com>
2023-06-13 16:32:02 +0300

    Title is build up in App.js

--- theme-switcher/src/App.js
+++ theme-switcher/src/App.js
@@ -1,6 +1,24 @@
-
 import './App.css';
+import { ThemeProvider, useTheme } from './ThemeContext';
+import Switch from "./Switch";
 
+const Title = ({ children }) => {
+  const { theme } = useTheme();
+
+  return (
+    <h2
+      style={{
+        color: theme === "light" ? "black" : "white",
+      }}
+      >
+        { children }
+    </h2>
+  );
+};
+
+
+
+}
 
 function App() {
   return (
