3209d904a7a5a9cec9e53f25c209effdc0da11c8
TKabakov <boteobg@gmail.com>
2023-06-15 07:48:20 +0300

    Some updates

--- theme-switcher/src/ThemeContext2.js
+++ theme-switcher/src/ThemeContext2.js
@@ -1,2 +1,20 @@
 import { createContext, useContext, useState} from 'react';
 
+const ThemeContext = createContext(null);
+
+export default function myApp(){
+   const [theme, setTheme] = useState('light');
+   
+   return (
+     <ThemeContext.Provider value={theme}>
+      <Form />
+      <label>
+        <input
+          type="checkbox"
+          checked={themr === 'dark'}
+          onChange={(e) => {
+          setTheme(e.terget.checked ? 'dark' : 'light')
+          }}
+        />
+     )
+}
