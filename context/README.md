be30e4ab7036166f32680f1affe906a368a22b57
TKabakov <boteobg@gmail.com>
2023-06-15 18:01:15 +0300

    UpdatingObject component is build up

--- context/README.md
+++ context/README.md
@@ -2,3 +2,5 @@ These are exercises that I make for practicing context in React.
 
 The source of the exercises is:
 https://react.dev/reference/react/useContext
+
+Example 1 of 5: Updating a value via context
--- context/src/App.js
+++ context/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import MyApp from './ThemeContext';
-
+import User from './UpdatingObject'
 function App() {
   return (
     <div className="App">
       <MyApp />
+      <User />
     </div>
   );
 }
