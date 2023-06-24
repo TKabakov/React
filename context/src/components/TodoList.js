41cf5869da9cb16b56a2abee573e87c51f777347
TKabakov <boteobg@gmail.com>
2023-06-24 07:02:31 +0300

    ToddList.js is done

--- context/src/components/TodoList.js
+++ context/src/components/TodoList.js
@@ -9,7 +9,17 @@ export default function TodoList({ todos, theme, tab}) {
 
     return(
         <div className={theme}>
-            
+            <p><b>Note:<code>filterTodos</code>is artificially slowed down!</b></p> 
+            <ul>
+                {visibleTodos.map(todo => (
+                    <li key={todo.id}>
+                        {todo.completed ?
+                        <s>{todo.text}</s> :
+                        todo.text
+                        }
+                    </li>
+                ))}
+            </ul>
         </div>
-    )
+    );
 }
