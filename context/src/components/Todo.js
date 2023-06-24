a53602e220005bab7e8b1685282062703571a872
TKabakov <boteobg@gmail.com>
2023-06-24 07:39:56 +0300

    All is set, but throws an error message

--- context/src/components/Todo.js
+++ context/src/components/Todo.js
@@ -7,33 +7,32 @@ const todos = createTodos;
 export default function  Todo() {
     const [tab, setTab] = useState('all');
     const [isDark, setIsDark] = useState(false);
-
     return (
-        <>
-            <button onClick={() => setTab ('all')}>
-                All
-            </button>
-            <button onClick={() => setTab ('active')}>
-                Active
-            </button>
-            <button onClick={() => setTab ('completed')}>
-                completed
-            </button>
-            <br />
-            <label>
-                <input
-                    type="checkbox"
-                    checked={isDark}
-                    onChange={ e => setIsDark(e.target.checked)}
-                />
-                Dark mode
-            </label>
-            <hr />
-            <TodoList
-                todos={todos}
-                tab={tab}
-                theme={isDark ? 'dark' : 'light'}
-            />          
-        </>
+      <>
+        <button onClick={() => setTab('all')}>
+          All
+        </button>
+        <button onClick={() => setTab('active')}>
+          Active
+        </button>
+        <button onClick={() => setTab('completed')}>
+          Completed
+        </button>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            checked={isDark}
+            onChange={e => setIsDark(e.target.checked)}
+          />
+          Dark mode
+        </label>
+        <hr />
+        <TodoList
+          todos={todos}
+          tab={tab}
+          theme={isDark ? 'dark' : 'light'}
+        />
+      </>
     );
-}
+  }
--- context/src/components/utils.js
+++ context/src/components/utils.js
@@ -1,32 +1,25 @@
-export function createTodos(){
+export function createTodos() {
     const todos = [];
-    for(let i=0;i<50;i++){
-    todos.push({
+    for (let i = 0; i < 50; i++) {
+      todos.push({
         id: i,
-        text: "Todo" + (i+1),
-        completed: Math.random()>0.5
-    });
-}
-
-return todos;
-}
-
-export function filterTodos(todos, tab){
-    console.log('[ARTIFICIIALLY SLOW] Filtering ' + todos.length + ' todoes for "' + tab + '" tab.');
-    let startTime = performance.now();
-    while (performance.now() - startTime < 500){
-      //Do nothing for 500 ms to emulate extremely slow code  
+        text: "Todo " + (i + 1),
+        completed: Math.random() > 0.5
+      });
     }
+    return todos;
+  }
+  
+  export function filterTodos(todos, tab) {
 
-    return todos.filter(todo => {
+    return (
+        todos.filter(todo => {
         if (tab === 'all') {
-            return true;
+          return true;
+        } else if (tab === 'active') {
+          return !todo.completed;
+        } else if (tab === 'completed') {
+          return todo.completed;
         }
-        else if(tab === 'active'){
-            return !todo.completed;
-        }
-        else if(tab === 'completed'){
-            return todo.completed;
-        }
-    });
-}
+      }));
+    }
