0dfa022ea3316befc90179ae1b790d5ce6d91859
TKabakov <boteobg@gmail.com>
2023-06-28 20:51:43 +0300

    ListOfGoals component is built up

--- hooks/src/components/Goals.js
+++ hooks/src/components/Goals.js
@@ -24,7 +24,15 @@ function GoalForm(props) {
 }
 
 function ListOfGoals(props) {
-
+    return (
+        <ul>
+            {props.allGoals.map((g) => (
+                <li key={g.goal}>
+                    <span>My goal is to {g.goal}, by {g.by}</span>
+                </li>
+            ))}
+        </ul>
+    );
 }
 
 export default function App() {
