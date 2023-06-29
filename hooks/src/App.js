1f5c7e30b7c6d7a34b287271957ef59e5da658c8
TKabakov <boteobg@gmail.com>
2023-06-29 20:06:31 +0300

    CiftCards components is created

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Objects from './components/Objects';
 import Goals from './components/Goals';
+import GiftCards from './components/GiftCrads';
 
 function App() {
   return (
     <div className="App">
       <Objects />
       <Goals />
+      <GiftCards />
     </div>
   );
 }
