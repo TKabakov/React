e0f073a1f39f17cf0c6a890bd88319abd91d8797
TKabakov <boteobg@gmail.com>
2023-07-04 06:25:17 +0300

    new component Form is added

---
+++ hooks/src/components/Artwork.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export default function Form(){
+    const [person, setPerson] = useState({
+      name:
+      artwork: {
+        title: ,
+        city: ,
+        image: ,
+      }  
+    })
+}
