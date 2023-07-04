d9b44a2534e3174f4b26a3d0e31901092e63f4c9
TKabakov <boteobg@gmail.com>
2023-07-04 06:27:34 +0300

    nested object is added to Form component

--- hooks/src/components/Artwork.js
+++ hooks/src/components/Artwork.js
@@ -2,11 +2,13 @@ import { useState } from 'react';
 
 export default function Form(){
     const [person, setPerson] = useState({
-      name:
+      name: 'Niki de Saint Phalle',
       artwork: {
-        title: ,
-        city: ,
-        image: ,
+        title: 'Blue Nana',
+        city: 'Hamburg',
+        image: 'https://i.imgur.com/Sd1AgUOm.jpg',
       }  
-    })
+    });
+
+    
 }
