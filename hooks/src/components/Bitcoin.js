1abc43b7523eeb5510c8b8ea08b5fa3e0a703a2f
TKabakov <boteobg@gmail.com>
2023-07-05 16:54:20 +0300

    Bitcoin component is ready

--- hooks/src/components/Bitcoin.js
+++ hooks/src/components/Bitcoin.js
@@ -9,4 +9,15 @@ export default function Bitcoin(){
             .then((jsonData) => setBtcData(jsonData.bpi.USD))
             .catch((error) => console.log(error));
     }, []);
+
+    return (
+        <>
+            <h1>Current BTC/USD data</h1>
+            <p>Code:{btcData.code}</p>
+            <p>Symbol: {btcData.symbol}</p>
+            <p>Rate: {btcData.rate}</p>
+            <p>Description: {btcData.description}</p>
+            <p>Rate Float: {btcData.rate_float}</p>
+        </>
+    );
 }
