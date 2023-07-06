fc69eb2263d458b30f894da96fb0952245b08e8c
TKabakov <boteobg@gmail.com>
2023-07-06 07:43:20 +0300

    Optimizing code in Bitcoin.js

--- hooks/src/components/Bitcoin.js
+++ hooks/src/components/Bitcoin.js
@@ -3,11 +3,15 @@ import { useState, useEffect } from "react";
 export default function Bitcoin(){
     const[btcData, setBtcData] = useState({});
 
-    useEffect(() => {
+  const fetchData = () => {
         fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`)
             .then((response) => response.json())
             .then((jsonData) => setBtcData(jsonData.bpi.USD))
             .catch((error) => console.log(error));
+  }
+
+    useEffect(() => {
+        fetchData();
     }, []);
 
     return (
