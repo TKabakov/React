d4bbf92099d31d60395d46205335c9e7d399bd9b
TKabakov <boteobg@gmail.com>
2023-07-07 19:11:34 +0300

    return statement is updated

--- hooks/src/components/CustomerData.js
+++ hooks/src/components/CustomerData.js
@@ -17,7 +17,7 @@ function CustomerData() {
   return Object.keys(user).length > 0 ? (
     <div style={{padding: "40px"}}>
       <h1>Customer data</h1>
-
+      <h2>Name: { user.results[0].name.first}</h2>
     </div>
   ) : (
     <h1>Data pending...</h1>
