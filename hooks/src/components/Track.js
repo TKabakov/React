a0bcd1626c64a58eb7124eb7c2162284ad19d717
TKabakov <boteobg@gmail.com>
2023-07-09 08:28:03 +0300

    const initialState is added

--- hooks/src/components/Track.js
+++ hooks/src/components/Track.js
@@ -4,11 +4,15 @@ import { useReducer } from 'react';
 const reducer = (state, action) => {
     if (action.type === "buy ingradients") return {money: state.money - 10};
     if (action.type === "sell a meal") return {money: state.money + 10};
-    if (actuon.type === "celebrity visit") return {money: state.money + 5000;}
+    if (actuon.type === "celebrity visit") return {money: state.money + 5000};
     return state;
 }
 
 function Track() {
+
+    const initialState = {money: 100};
+    const [state, dipatch] = useReducer(reducer, initialState);
+    
   return (
     <div>
       
