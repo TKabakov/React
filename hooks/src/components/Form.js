71a33967e1461036c117b6c872cda5f2655b3d92
TKabakov <boteobg@gmail.com>
2023-07-12 21:19:30 +0300

    handleButtonClick() and handleInputChange(e) are added to Form function

--- hooks/src/components/Form.js
+++ hooks/src/components/Form.js
@@ -23,5 +23,27 @@ const initialState = { name: 'Taylor', age: 42 };
 export default function Form() {
     const [state, dispatch] = useReducer(reducer, initialState);
 
-    
+    function handleButtonClick(){
+        dispatch({ type: 'incremented_age' });
+    }
+
+    function handleInputChange(e) {
+        dispatch({
+            type: 'changed_name',
+            nextName: e.target.value
+        });
+    }
+
+    return (
+        <>
+            <input
+                value={state.name}
+                onChange={handleInputChange}
+            ></input>
+            <button onClick={handleButtonClick}>
+                Incremented age
+            </button>
+            <p>Hello, { state.name }. You are {state.age}. </p>
+        </>
+    );
 }
