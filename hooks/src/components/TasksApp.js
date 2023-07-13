9e64d1f5098436ec86b4f822a4adf16bc325aeba
TKabakov <boteobg@gmail.com>
2023-07-13 11:03:13 +0300

    reducerfunction is created

---
+++ hooks/src/components/TasksApp.js
@@ -0,0 +1,22 @@
+import { useReducer  } from 'react';
+
+function reducer (state, action){
+  switch( action.type) {
+    case 'incremented_age': {
+      return {
+        name: state.name,
+        age: state.age + 1
+      };
+    }
+    
+    case 'changed_name': {
+      return {
+        name: action.nextName,
+        age: state.age
+      };
+    }
+      
+    }
+  }
+  }
+}
