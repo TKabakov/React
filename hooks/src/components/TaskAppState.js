227eec75d7108cfcdf2187376a290eafcd5164ba
TKabakov <boteobg@gmail.com>
2023-07-16 19:06:02 +0300

    if statement is replaced with a swith statement

--- hooks/src/components/TaskAppState.js
+++ hooks/src/components/TaskAppState.js
@@ -6,66 +6,48 @@ export default function TaskAppState() {
   const [tasks, setTasks] = useState(initialTasks);
 
   function taskReducer (tasks, action){
-    if(action.type === 'added'){
-        return [
-            ...tasks,
-            {
-              id: action.id,
-              text: action.text,
-              done: false,
-            },
-          ];
-    }
-    else if( action.type === 'changed'){
-        return tasks.map((t) => {
-            if (t.id === action.task.id) {
-              return action.task;
-            } else {
-              return t;
-            }
-          }) ;
-    }
-
-    else if (action.type === 'deleted') {
-        return tasks.filter((t) => t.id !== action.id);
-    }
-    else {
-        throw Error('Unknown action: ' + action.type);
+    switch (action.type){
+       case 'added':{
+            return [
+                ...tasks,
+                {
+                  id: action.id,
+                  text: action.text,
+                  done: false,
+                },
+              ];
+        }
+        case 'changed': {
+            return tasks.map((t) => {
+                if (t.id === action.task.id) {
+                  return action.task;
+                } else {
+                  return t;
+                }
+              }) ;
+        }
+        case 'deleted' : {
+            return tasks.filter((t) => t.id !== action.id);
+        }
+        default: {
+            throw Error('Unknown action: ' + action.type);
+        }
     }
   }
-  
+
   function handleAddTask(text) {
     dispatch({
         type: 'added',
         id: nextId++,
         text: text,
     });
- /*   setTasks([
-      ...tasks,
-      {
-        id: nextId++,
-        text: text,
-        done: false,
-      },
-    ])*/
-  
   }
 
   function handleChangeTask(task) {
     dispatch({
         type: 'changed',
         task: task,
-    });
-/*    setTasks(
-      tasks.map((t) => {
-        if (t.id === task.id) {
-          return task;
-        } else {
-          return t;
-        }
-      })
-    )*/
-    
+    });   
   }
 
   function handleDeleteTask(taskId) {
@@ -73,9 +55,6 @@ export default function TaskAppState() {
         tpe: 'deleted',
         id: taskId,
     });
-
-/*    setTasks(tasks.filter((t) => t.id !== taskId));
-*/
   }
 
   return (
