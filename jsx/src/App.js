f9ff514dada594615c24482aef0fb1689b125262
TKabakov <boteobg@gmail.com>
2023-07-22 20:07:13 +0300

    Fragment is included in App.js

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import { RadioGroup, RadioOption } from './Radio';
 import { useState } from 'react';
-
+import { Fragment } from 'react'
 function App() {
 
   const [selected, setSelected] = useState('');
 
   return (
-    <div className="App">
+    <Fragment>
      <h2>How do you hear about Little Lemon?</h2>
      <RadioGroup onChange={setSelected} selected={selected}>
         <RadioOption value='social_media'>Social Media</RadioOption>
@@ -17,7 +17,7 @@ function App() {
      </RadioGroup>
      <button 
         disabled={!selected} onClick={() => {alert("Thank you for submitting your choice")}}>Submit</button>
-    </div>
+    </Fragment>
   );
 }
 
