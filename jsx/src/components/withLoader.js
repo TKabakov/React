cdc3e54023c909d213717865eef1bb24203e6765
TKabakov <boteobg@gmail.com>
2023-07-26 19:55:08 +0300

    return statement with WrappedComponent is added

--- jsx/src/components/withLoader.js
+++ jsx/src/components/withLoader.js
@@ -4,6 +4,14 @@ const withLoader = (WrappedComponent) => {
     return (props) => {
         const [ loading, setLoading] = useState(true);
     
-        setTimeout(() => )
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+
+        return (
+            <div>
+                {loading ? <p>Loading ...</p> : WrappedComponent {...props}}
+            </div>
+        )
     }
 } 
