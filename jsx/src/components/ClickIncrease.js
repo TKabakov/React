6f4f6e7a79d5717f94db33cb925cbccab3da3a1a
TKabakov <boteobg@gmail.com>
2023-07-27 17:18:44 +0300

    HooverIncrease component is ready

--- jsx/src/components/ClickIncrease.js
+++ jsx/src/components/ClickIncrease.js
@@ -1,12 +1,15 @@
 function ClickIncrease() {
-  const [fontsize, setFontSize] = useState(10);
+  const [fontSize, setFontSize] = useState(10);
   
   return(
       	<div>
           <button onClick={() => setFontSize((size) => size + 1)}>
             Increase with click
           </button>
-          <p style={{ frontSize }}>Size of font in onClick function: {fontsize}</p>
+          <p style={{ fontSize }}>
+            Size of font in onClick function: 
+            {fontSize}
+          </p>
         </div>
     );
 }
---
+++ jsx/src/components/HooverIncrease.js
@@ -0,0 +1,17 @@
+function HooverIncrease() {
+    const [fontSize, setFontSize] = useState(10);
+    
+    return(
+            <div>
+            <button onMouseOver={() => setFontSize((size) => size + 1)}>
+              Increase on mouse hoover
+            </button>
+            <p style={{ fontSize }}>
+                Size of font in onMouseOver function: 
+                {fontSize}
+            </p>
+          </div>
+      );
+  }
+  
+  export default HooverIncrease;
