c8e2612c15e5af70301bb775d6fe18d0f6ef0a83
TKabakov <boteobg@gmail.com>
2023-07-29 07:59:23 +0300

    state hook counter is added to withCounter

--- jsx/src/components/withCounter.js
+++ jsx/src/components/withCounter.js
@@ -1,6 +1,15 @@
 const UpdatedComponent = (OriginalComponent) => {
   function NewComponent(props){
-    return <OriginalComponent name="LogRocket"/>
+    const [counter, setCounter] = useState(10)
+    return(
+        <OriginalComponent
+        name="LogRocket"
+        counter ={counter}
+        incrementCounter={
+          () => setCounte((count) => count +1)
+        }
+        />
+      );
   }
   return NewComponent;
 };
