1b650c63fe58081194d377537acd5ca5cac09870
TKabakov <boteobg@gmail.com>
2023-07-31 19:23:06 +0300

    textAreaPlaceholder variable and handleSubmit function are created

--- react-testing/src/FeedbackForm.js
+++ react-testing/src/FeedbackForm.js
@@ -5,6 +5,17 @@ function FeedbackForm(){
 const [score, setScore] = useState("10");
 const [comment, setComment] = useState("");
 
+const isDisabled = Number(score) < 5 && comment.length <= 10;
+
+const textAreaPlaceholder = isDisabled
+    ? "Please, provide a comment explaining why the experience was not good. Minimum length is 10 characters"
+    : "Optional feedback";
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({score, comment});
+};
+
     return
 }
 
