55f006d0b5af7b13921d68c5ff47b0b4143daf1f
TKabakov <boteobg@gmail.com>
2023-08-14 16:49:42 +0300

    const ToDo is created in ToDo.js

--- rendering-lists/src/components/ToDo.js
+++ rendering-lists/src/components/ToDo.js
@@ -1,2 +1,15 @@
 import { useState } from 'react';
 
+const ToDo = props => (
+    <tr>
+        <td>
+            <label>{props.id}</label>
+        </td>
+        <td>
+            <input />
+        </td>
+        <td>
+            <label>{props.createdAt}</label>
+        </td>
+    </tr>
+)
