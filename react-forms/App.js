0ba81eaf3fa2eccee1b07c0f4895e6508a79a2c8
TKabakov <boteobg@gmail.com>
2023-08-16 12:28:39 +0300

    App.js in Forms in updated

---
+++ react-forms/App.js
@@ -0,0 +1,26 @@
+
+import Form from "./components/Form";
+import FormControlled from "./components/FormControlled";
+import Feedback from "./components/Feedback";
+import RegForm from "./components/RegForm";
+import MyForm from "./components/MyForm";
+import Input from "./components/Input";
+import ToDoList from "./components/ToDo"
+import './App.css';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <Form />
+      <FormControlled />
+      <Feedback />
+      <RegForm />
+      <MyForm />
+      <Input />
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
