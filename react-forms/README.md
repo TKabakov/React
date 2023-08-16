8f35182b905acdfb7cd35551aa85337c03d0997a
TKabakov <boteobg@gmail.com>
2023-08-16 22:46:20 +0300

    README.md and App.js more updates

--- react-forms/README.md
+++ react-forms/README.md
@@ -1,3 +1,3 @@
 This app demonstrate some features of Forms in React.
-1. Useage of uncontrolled forms with useRef (From.js).
-2. Controlled form - the form accepts its value as a prop and a callback to change that value. The input value is saved as a state (FromControlled.js).
+1. Useage of uncontrolled forms with useRef (From.js).![Alt text](image.png)
+2. Controlled form - the form accepts its value as a prop and a callback to change that value. The input value is saved as a state (FromControlled.js).![Alt text](image-1.png)
--- react-forms/src/App.js
+++ react-forms/src/App.js
@@ -11,10 +11,10 @@ function App() {
  
   return (
     <div className="App">
-      <h2>Uncontrolled forms</h2>
+      <h2>Uncontrolled form:</h2>
         <Form />
-      <h2>Controlled forms</h2>
-      <p>the form accepts its value as a prop and a callback to change that value. The input value is saved as a state</p>
+      <h2>Controlled form:</h2>
+        <p>The form accepts its value as a prop and a callback to change that value. The input value is saved in a state.</p>
         <FormControlled />
       <Feedback />
       <RegForm />
