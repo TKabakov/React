97d8770ad106a6dea11b5df6f1b7ec3cf6d9a58f
TKabakov <boteobg@gmail.com>
2023-08-16 15:16:15 +0300

    App.js is updated

--- rendering-lists/src/App.js
+++ rendering-lists/src/App.js
@@ -2,14 +2,7 @@ import Recipes from "./components/Salads";
 import RecipeList from "./components/Recipe";
 import Recipe from "./components/Recipe2";
 import List from "./components/Scientists";
-import Form from "./components/Form";
-import FormControlled from "./components/FormControlled";
-import Feedback from "./components/Feedback";
-import RegForm from "./components/RegForm";
-import MyForm from "./components/MyForm";
-import Input from "./components/Input";
 import Sculpture from "./components/Sculpture";
-import ToDoList from "./components/ToDo"
 import './App.css';
 
 function App() {
@@ -20,14 +13,7 @@ function App() {
       <RecipeList />
       <Recipe />
       <List />
-      <Form />
-      <FormControlled />
-      <Feedback />
-      <RegForm />
-      <MyForm />
-      <Input />
       <Sculpture />
-      <ToDoList />
     </div>
   );
 }
