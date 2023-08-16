60131ed05c08aca59b9dd4d9cf759a75f070dadc
TKabakov <boteobg@gmail.com>
2023-08-16 18:22:02 +0300

    react-forms app is ready

--- react-forms/src/App.js
+++ react-forms/src/App.js
@@ -4,7 +4,7 @@ import Feedback from "./components/Feedback";
 import RegForm from "./components/RegForm";
 import MyForm from "./components/MyForm";
 import Input from "./components/Input";
-import ToDoList from "./components/ToDo"
+import ToDoList from "./components/ToDo";
 import './App.css';
 
 function App() {
