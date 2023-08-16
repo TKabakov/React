4441367a4c91e7e01551749944917737ad20d2f0
TKabakov <boteobg@gmail.com>
2023-08-16 07:42:20 +0300

    prepare a new app rendering forms

---
+++ react-forms/components/FormControlled.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState } from 'react';
+
+function FormControlled() {
+    const [value, setValue] = useState('');
+    
+    const handleChange = (e) => {
+        setValue(e.target.value)
+    }
+    return (
+    <div>
+      <form>
+        <input
+            value={value}
+            onChange={handleChange}
+            text='text'
+        />
+      </form>
+    </div>
+  );
+}
+
+export default FormControlled
---
+++ react-forms/components/Input.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function Input(){
+  const [firstName, setFirstName] = useState("");
+  const [age, setAge] =useState("");
+  const ageAsNumber = Number(age);
+
+    return(
+    <>
+      <label>
+        Firts name:
+        <input
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Your age:
+        <input
+          value={age}
+          onChange={e => setAge(e.target.value)}
+          type="number"
+          />
+            
+        <button onClick={() => setAge(ageAsNumber + 10)}>
+            Add 10 years
+        </button>
+        </label>
+      
+      {firstName !== "" && 
+        <p> Your name is {firstName}.</p>
+      }
+      {ageAsNumber > 0 &&
+        <p>Your are is: {ageAsNumber}.</p>
+      }
+     </> 
+    );
+}
+
+export default Input;
