d6cb2f1c9f0672c190489e9b6310ca8a091e91c9
TKabakov <boteobg@gmail.com>
2023-08-19 13:55:15 +0300

    text inpit is added wrap in a label

--- react-forms/src/components/Form2.js
+++ react-forms/src/components/Form2.js
@@ -1,3 +1,9 @@
 export default function Form2(){
-  
+  return (
+    <div>
+        <lable>
+            <input name="myInput" defaultValue="Some initial value" placeholder="More value"/>
+        </lable>
+    </div>
+  )
 }
