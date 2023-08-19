1e08ca3de41d09b4f910c8a06e14ebce4af3d7da
TKabakov <boteobg@gmail.com>
2023-08-19 14:09:39 +0300

    Radio input is added

--- react-forms/src/components/Form2.js
+++ react-forms/src/components/Form2.js
@@ -2,10 +2,35 @@ export default function Form2(){
   return (
     <div>
         <lable>
+            Text input:
             <input name="myInput" defaultValue="Some initial value" placeholder="More value"/>
         </lable>
+        <hr />
         <label>
+            Checkbox:
             <input type="checkbox" name="myCheckbox" />
+        </label>
+        <hr />
+        <label>
+            <p>
+                Radio buttons:
+                <hr />
+                <label>  
+                    <input type="radio" name="myRadio" value="optipn 1" />
+                    Option 1
+                </label>
+                <hr />                
+                <label>
+                    <input type="radio" name="myRadio" value="optipn 2" />
+                    Option 2
+                </label>
+                <hr />              
+                <label>
+                    <input type="radio" name="myRadio" value="optipn 3" />
+                    Optipn 3
+                </label>
+                <hr />
+            </p>
         </label>
     </div>
   )
