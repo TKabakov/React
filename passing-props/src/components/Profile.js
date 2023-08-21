c2dedc201ced8c26866ee3f12abd5e914c90afa1
TKabakov <boteobg@gmail.com>
2023-08-21 14:47:58 +0300

    Props person and size are passed to Data componenet

--- passing-props/src/components/Profile.js
+++ passing-props/src/components/Profile.js
@@ -1,57 +1,60 @@
 import { getImageUrl } from './utils.js';
 
+function Data({person,size}){
+    return(
+        <div>
+        <h2>{person.name}</h2>
+         <img
+            className="avatar"
+            src={getImageUrl(person)}
+            alt={person.name}
+            width={size}
+            height={70}
+         />
+        <ul>
+        <li>
+            <b>Profession: </b> 
+            {person.profession}
+        </li>
+        <li>
+            <b>Awards:{person.awardNumber}</b> 
+            {person.awardText}
+        </li>
+        <li>
+            <b>Discovered: </b>
+            {person.discovered}
+        </li>
+        </ul>
+    </div>
+    )
+}
+
 export default function Gallery() {
   return (
     <div>
-      <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('szV5sdG')}
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b> 
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b> 
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('YfeOqp2')}
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b> 
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b> 
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+ <h1>Notable Scientists</h1>
+    <Data
+        size={70}
+        person={{
+            name:"Maria Skłodowska-Curie",
+            imageId:'szV5sdG',
+            profession:"physicist and chemist",
+            awardNumber:4 ,
+            awardText:"(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)",
+            discovered:"polonium (element)",
+        }}
+    />
+    <Data
+        size={70}
+        person={{
+            name:"Katsuko Saruhashi",
+            imageId:'YfeOqp2',
+            profession:"geochemist",
+            awardNumber:2 ,
+            awardText:"(Miyake Prize for geochemistry, Tanaka Prize)",
+            discovered:"a method for measuring carbon dioxide in seawater",
+        }}
+    />
     </div>
   );
 }
