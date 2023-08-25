4f014419f87049dfd8c6ad3896e077f3aa188fe9
TKabakov <boteobg@gmail.com>
2023-08-25 06:24:59 +0300

    More touches on README

--- theme-switcher/src/Context/ThemeContext.js
+++ theme-switcher/src/Context/ThemeContext.js
@@ -5,7 +5,7 @@ const ThemeContext = createContext(undefined);
 export const ThemeProvider = ({ children }) => {
 
     const [theme, setTheme] = useState("light");
-
+//default value can be undefined or null also
     return (
         <ThemeContext.Provider
             value={{
