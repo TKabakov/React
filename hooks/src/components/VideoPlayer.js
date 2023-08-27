9e0114c9b37fb2e5871e3b4ac600092c064f5018
TKabakov <boteobg@gmail.com>
2023-08-27 20:19:07 +0300

    Child Player is added to parent VideoPlayer wirh teo props

--- hooks/src/components/VideoPlayer.js
+++ hooks/src/components/VideoPlayer.js
@@ -13,8 +13,11 @@ export default VideoPlayer(){
             <button onClick = {() => setIsPlaying(!isPlaying)}>
                 {isPlaying ? 'Pause' : 'Play'}
             </button>
-            
+            <Player
+                isPlaying={isPlaying}
+                src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+            />
         </div>
-    )
+    );
 
 }
