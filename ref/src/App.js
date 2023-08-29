62bdf529848642cd3487eee22d445aa3b4da1ba1
TKabakov <boteobg@gmail.com>
2023-08-29 15:25:07 +0300

    function handleClick is added inside Counter

--- ref/src/App.js
+++ ref/src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import Counter from './Counter';
 
 function App() {
+  let ref = useRef(0);
+
+  function handleClick() {
+    ref.current = ref.current + 1;
+    alert('You clicked ' + ref.current + ' times!');
+  }
   return (
     <div className="App">
         <Counter />
