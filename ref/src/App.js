f148a511ad6e8bc682c08c7cc33060ed15a305cc
TKabakov <boteobg@gmail.com>
2023-08-30 17:18:30 +0300

    Stopwatch is added to App.js

--- ref/src/App.js
+++ ref/src/App.js
@@ -1,14 +1,15 @@
 
 import './App.css';
-import Counter from './Counter';
+import Counter from './components/Counter';
+import Stopwatch from './components/Stopwatch';
 
 function App() {
   return (
     <div>
       <Counter />
+      <Stopwatch />
     </div>
   )
 }
-
 
 export default App;
