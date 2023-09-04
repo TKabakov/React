a157ac70bd13c59e2cd2a6851d8fb3ec2dddac9c
TKabakov <boteobg@gmail.com>
2023-09-04 19:54:53 +0300

    passing-props is updated

--- passing-props/src/App.js
+++ passing-props/src/App.js
@@ -2,6 +2,8 @@ import Heading from "./components/Heading";
 import Gallery from "./components/Profile";
 import Profile from "./components/Avatar";
 import Profile2 from "./components/Profile2";
+import Expression from "./components/Expression";
+import Expression2 from "./components/Expression2";
 
 function Header () {
     return <h1>Hello world</h1>
@@ -19,7 +21,15 @@ function App() {
             <Gallery />
             <Profile />
             <Profile2 />
-
+            <Header />
+            <Expression
+                toggleBoolean={!bool}
+            />
+            <Expression2
+                toggleBoolean={!bool}
+                math = {(20 + 30) / 10}
+                str = {str1+" another "+"string"}
+            />
         </div>
     );
 };
