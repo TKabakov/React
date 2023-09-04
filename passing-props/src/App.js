15fb6dd31df6daca826f9738aedf327ba9c38216
TKabakov <boteobg@gmail.com>
2023-09-04 19:50:01 +0300

    passing-props is updated

--- passing-props/src/App.js
+++ passing-props/src/App.js
@@ -3,7 +3,15 @@ import Gallery from "./components/Profile";
 import Profile from "./components/Avatar";
 import Profile2 from "./components/Profile2";
 
+function Header () {
+    return <h1>Hello world</h1>
+  }
+  
+const bool=true;
+const str1="just";
+
 function App() {
+
     return (
         <div className="App">
             <Heading firstName="Bob" lastName="Doe"/>
