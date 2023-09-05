d80337fd2713debe2af361bfbfe1ec6f4c86f048
TKabakov <boteobg@gmail.com>
2023-09-05 18:54:37 +0300

    A namee export of Profile

--- creating-components/src/components/Gallery.js
+++ creating-components/src/components/Gallery.js
@@ -1,20 +1,14 @@
-// Move me to Profile.js!
-
-export function Profile() {
-    return (
-      <img
-        src="https://i.imgur.com/QIrZWGIs.jpg"
-        alt="Alan L. Hart"
-      />
-    );
-  }
+import { Profile } from "./Profile";
+//named import
   
   export default function Gallery() {
     return (
       <section>
         <h1>Amazing scientists</h1>
         <Profile />
+        <br/>
         <Profile />
+        <br/>
         <Profile />
       </section>
     );
