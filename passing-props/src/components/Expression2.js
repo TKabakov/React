5c9f0fb61cf60ca4daf8c77d09863d5ca1c4b013
TKabakov <boteobg@gmail.com>
2023-09-06 15:25:42 +0300

    props is destructured for Expression2

--- passing-props/src/components/Expression2.js
+++ passing-props/src/components/Expression2.js
@@ -1,14 +1,26 @@
 //The math prop is there to show that you can add arithmetic operators and numbers inside JSX, and it will be evaluated just like it does in plain JavaScript. 
 //The str prop is there to show that you can concatenate strings, as well as strings and variables – which is shown by adding string literals of “ another ” and “string” to the str1 variable.
 
-function Expression2 (props) {
+//function Expression2 (props) {
+//   return (
+//       <div>
+//          <h2>
+//                The value of the toggleBoolean prop is: {props.toggleBoolean.toString()}
+//           </h2>
+//               <p>The value of the main prop is: <em>{props.math}</em></p>
+//                <p>The value of str is: <em>{props.str}</em></p>
+//        </div>
+//   );
+//};
+
+function Expression2 ({toggleBoolean}) {
     return (
         <div>
             <h2>
-            The value of the toggleBoolean prop is: {props.toggleBoolean.toString()}
+                The value of the toggleBoolean prop is: {toggleBoolean.toString()}
             </h2>
-            <p>The value of the main prop is: <em>{props.math}</em></p>
-            <p>The value of str is: <em>{props.str}</em></p>
+                <p>The value of the main prop is: <em>{toggleBoolean.math}</em></p>
+                <p>The value of str is: <em>{toggleBoolean.str}</em></p>
         </div>
     );
 };
