7313d8da8d55f1357a3b0bcb59a9ff6bef8da1e9
TKabakov <boteobg@gmail.com>
2023-09-09 18:35:28 +0300

    Cups is updated

--- rendering-lists/src/components/Cups.js
+++ rendering-lists/src/components/Cups.js
@@ -5,11 +5,12 @@ function Cup({guest}) {
 }
 
 export default function TeaSet() {
+    let cups = [];
+    for (let i=1; i<=12; i=i+2){
+        cups.push(<Cup key={1} guest={i} />);
+    }
+
   return (
-    <>
-      <Cup guest={1}/>
-      <Cup guest={2}/>
-      <Cup guest={3}/>
-    </>
+        cups
   );
 }
