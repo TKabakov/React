2bbbaea7a69bf3a8c362703e14251b2f976b8a50
TKabakov <boteobg@gmail.com>
2023-09-13 18:19:41 +0300

    Score component is updated

--- manage-state-in-react/src/components/Score.js
+++ manage-state-in-react/src/components/Score.js
@@ -4,25 +4,18 @@ export default function Counter() {
   const [score, setScore] = useState(0);
 
   function increment() {
-    import { useState } from 'react';
+    setScore(score + 1);
+  }
 
-    export default function Counter() {
-      const [score, setScore] = useState(0);
-    
-      function increment() {
-        setScore(score + 1);
-      }
-    
-      return (
-        <>
-          <button onClick={() => increment()}>+1</button>
-          <button onClick={() => {
-            increment();
-            increment();
-            increment();
-          }}>+3</button>
-          <h1>Score: {score}</h1>
-        </>
-      )
-    }
-    
+  return (
+    <>
+      <button onClick={() => increment()}>+1</button>
+      <button onClick={() => {
+        increment();
+        increment();
+        increment();
+      }}>+3</button>
+      <h1>Score: {score}</h1>
+    </>
+  )
+}
