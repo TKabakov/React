f023121049551969302df39cc180ab866d26a326
TKabakov <boteobg@gmail.com>
2023-09-14 22:51:02 +0300

    Component PersonalData is updated

--- manage-state-in-react/src/components/PersonalData.js
+++ manage-state-in-react/src/components/PersonalData.js
@@ -8,4 +8,42 @@ export default function Data(){
         email:"bhepworth@sculpture.com"
     });
 
+    function handleFirstNameChange(e){
+        person.firstName=e.target.value
+        }
+    }
+
+    function handleLastNameChange(e){
+        person.lastName=e.target.value
+    }
+
+    function handleEmailChange(e){
+        person.email=e.target.value
+    }
+
+    return(
+        <div>
+       <label>
+            First Name:
+            <input
+                value={person.firstName}
+                onChange={handleFirstNameChange}>
+            </input>
+        </label>
+        <label>
+            Last Name:
+            <input
+                value={person.lasttName}
+                onChange={handleLastNameChange}>                </input>
+        </label>
+        <label>
+            Email:
+            <input
+                value={person.email}
+                onChange={handleEmailChange}>
+            </input>
+        </label>
+        </div>
+ >
+    )
 }
