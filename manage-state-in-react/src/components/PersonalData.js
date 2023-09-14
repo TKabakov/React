6f55e11185f75e47e872ff1070586b8a2e5c5b44
TKabakov <boteobg@gmail.com>
2023-09-14 22:33:52 +0300

    Component PersonalData is created

---
+++ manage-state-in-react/src/components/PersonalData.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Data(){
+
+    const [person, setPerson] = useState({
+        firstName: "Barbara" ,
+        lastName: "Hpworth",
+        email:"bhepworth@sculpture.com"
+    });
+
+}
