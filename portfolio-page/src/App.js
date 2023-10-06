4822dfca9cc638dc7f688af7b62453daa050b04d
TKabakov <boteobg@gmail.com>
2023-10-06 22:50:12 +0300

    Some libraries are installed

--- portfolio-page/src/App.js
+++ portfolio-page/src/App.js
@@ -4,7 +4,7 @@ import LandingSection from "./components/LandingSection";
 import ProjectsSection from "./components/ProjectsSection";
 import ContactMeSection from "./components/ContactMeSection";
 import Footer from "./components/Footer";
-import { AlertProvider } from "./context/alertContext";
+import { AlertProvider } from "./context/alertContext.js"
 import Alert from "./components/Alert";
 
 function App() {
