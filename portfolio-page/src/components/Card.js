03861d5bba1920ec1be0b7126504cef873fb375b
TKabakov <boteobg@gmail.com>
2023-10-07 21:53:56 +0300

    Card component is updated

--- portfolio-page/src/components/Card.js
+++ portfolio-page/src/components/Card.js
@@ -7,6 +7,14 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
+<VStack>
+  <Image
+    borderRadius="sm"
+    boxSize="150px"
+    src={imageSrc}
+    />
+    <Heading>{title}</Heading>
+</VStack>
   return null;
 };
 
