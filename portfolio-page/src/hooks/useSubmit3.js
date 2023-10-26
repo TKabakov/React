393bd192f79b356e898a65d624e2029ccaa4f2d7
TKabakov <boteobg@gmail.com>
2023-10-26 19:59:19 +0300

    useSubmit3 is updated

--- portfolio-page/src/hooks/useSubmit3.js
+++ portfolio-page/src/hooks/useSubmit3.js
@@ -1,11 +1,20 @@
 import {useState} from "react";
 
-function wait(){
+function wait1(){
     return new Promise(
         (resolve,rejects) => {
             setTimeout(() => {
                 reject("404")
             }, 2000)
-        }
-    )
+        })
 }
+
+function wait2(){
+    return new Promise(
+        (resolve,rejects) => {
+            setTimeout(() => {
+                resolve("Hello!")
+            }, 2000)
+        })
+}
+
