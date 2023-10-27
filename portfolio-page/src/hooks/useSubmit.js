91b72b2404eccef6a00689d6788f69ff698b97dd
TKabakov <boteobg@gmail.com>
2023-10-27 19:25:43 +0300

    useSubmit is updated

--- portfolio-page/src/hooks/useSubmit.js
+++ portfolio-page/src/hooks/useSubmit.js
@@ -19,12 +19,13 @@ const useSubmit = () => {
     try {
       await wait(2000);
       if (random < 0.5) {
-        throw new Error("Something went wrong");
-      } else {
       setResponse({
         type: 'success',
         message: `Thanks for your submission ${data.firstName}, we will get back to you shortly!`,
       })}
+      else{
+        throw new Error("Something went wrong");
+      }
     } catch (error) {
       setResponse({
         type: 'error',
