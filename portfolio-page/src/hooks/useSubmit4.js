eb0565b41449097dc665eb3f67f8e06cab35f08e
TKabakov <boteobg@gmail.com>
2023-10-29 22:03:06 +0200

    more updates on useSubmit4

--- portfolio-page/src/hooks/useSubmit4.js
+++ portfolio-page/src/hooks/useSubmit4.js
@@ -1,29 +1,37 @@
 
-const wait = new Promise((resolve, reject) => {
-    
-    const [isLoading, setLoading] = useState(false);
-    const [response, setResponse] = useState(null);let message = ""
-    
-    let random = Math.random();
-    
-    setTimeout(() => {
-        if (random > 0.5){
-        resolve(random, message);
-        } else {
-        reject(random);
-        }   
-    }, 2000)
-});
+const useSubmit = () => {
 
-wait
-.then((message) => {
-    message = "Thanks for your submission, we will get back to you shortly!";
-    console.log(message)
-})
-.catch(() => {
-    message = "Something went wrong, please try again later";
-    console.log(message)
-})
-.finally(() => {
-    console.log("All done");
-})
+    const [isLoading, setLoading] = useState(false);
+    const [response, setResponse] = useState(null);
+
+    const wait = new Promise((resolve, reject) => {
+    
+        let message = ""
+        
+        let random = Math.random();
+        
+        setTimeout(() => {
+            if (random > 0.5){
+            resolve(random, message);
+            } else {
+            reject(random);
+            }   
+        }, 2000)
+    });
+    
+    wait
+    .then((message) => {
+        message = "Thanks for your submission, we will get back to you shortly!";
+        console.log(message)
+    })
+    .catch(() => {
+        message = "Something went wrong, please try again later";
+        console.log(message)
+    })
+    .finally(() => {
+        console.log("All done");
+    })
+    
+
+}
+
