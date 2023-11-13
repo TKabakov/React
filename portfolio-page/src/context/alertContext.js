c5d76dac2c88dda1c73f44aa64d2659b200e3762
TKabakov <boteobg@gmail.com>
2023-11-13 22:24:10 +0200

    alertContext is updated

--- portfolio-page/src/context/alertContext.js
+++ portfolio-page/src/context/alertContext.js
@@ -23,7 +23,7 @@ export const AlertProvider = ({ children }) => {
     <AlertContext.Provider
       value={{
         ...state,
-        onOpen: (type, message) => setState({ isOpen: true, type, message }),
+        onOpen: (type, message1) => {setState({ isOpen: true, type: response.type, message1:response.message1 })},
         onClose: () => setState({ isOpen: false, type: '', message: '' }),
       }}
     >
