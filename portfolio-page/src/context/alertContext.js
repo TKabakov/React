370e3eb42353990029bb8659e8d6a644c81d9661
TKabakov <boteobg@gmail.com>
2023-11-14 19:24:00 +0200

    AlertContext is created

--- portfolio-page/src/context/alertContext.js
+++ portfolio-page/src/context/alertContext.js
@@ -1,46 +1,5 @@
 import {createContext, useContext, useState} from "react";
 import useSubmit from "../hooks/useSubmit";
 
-const AlertContext = createContext(undefined);
+export const AlertContext = createContext(undefined);
 
-
-
-export const AlertProvider = ({ children }) => {
-
-  const {isLoading, response, submit} = useSubmit();
-
-  const [state, setState] = useState({
-    isOpen: false,
-    // Type can be either "success" or "error"
-    type: "",
-    // Message to be displayed, can be any string
-    title: "",
-    message1: "",
-    message2: "",
-  });
-
-  const value = ({response}) => {
-    setState({
-      ...state,
-      type: response.type,
-    })
-  }
-
-  console.log(response.type);
-  console.log(state);
-
-  return (
-    <AlertContext.Provider
-      value={{
-        ...state,
-        onOpen: (type, message1) => {setState({ isOpen: true, type: '', message1:'' })},
-        onClose: () => setState({ isOpen: false, type: '', message: '' }),
-      }}
-    >
-      {children}
-
-    </AlertContext.Provider>
-  );
-};
-
-export const useAlertContext = () => useContext(AlertContext);
