eaa1d2115223ee644105126a8b7182750c9db3bc
TKabakov <boteobg@gmail.com>
2023-11-30 21:37:28 +0200

    onSubmit() is updated

--- portfolio-page/src/components/ContactMeSection.js
+++ portfolio-page/src/components/ContactMeSection.js
@@ -53,8 +53,6 @@ const ContactMeSection = forwardRef((props, ref) => {
       ...state,
       type: response.type,
       message: response.message1,
-      onOpen: (type, message) => setState({ isOpen: true, type, message }),
-      onClose: () => setState({ isOpen: false, type: '', message: '' }),
       });
    },
 // state is updated but it gives empty type and empty message
