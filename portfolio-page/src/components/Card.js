59fc045214a82841695e7338885a36961641a3cb
TKabakov <boteobg@gmail.com>
2023-12-16 12:31:59 +0200

    some clean-ups in Card

--- portfolio-page/src/components/Card.js
+++ portfolio-page/src/components/Card.js
@@ -1,4 +1,4 @@
-import { Box, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Heading, HStack, Image, Text  } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
